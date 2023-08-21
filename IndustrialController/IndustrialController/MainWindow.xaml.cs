ddee04592702a77982cb8a9120c961bbfd2f6f57
huangpangzi <little7313@163.com>
2023-08-21 21:42:12 +0800

    确认使用数据去当作列表

--- IndustrialController/IndustrialController/MainWindow.xaml.cs
+++ IndustrialController/IndustrialController/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace IndustrialController
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
     }
 }
